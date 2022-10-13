0765fdd324d7cf1f16d8a48ec25fe73468389038
Abdumalik07 <koktash.sarkent@gmail.com>
2022-10-14 02:09:13 +0600

    created packages controller,dto,serviceImpl,security,jwt,repositories,mapper. And created classes like a AuthController,UserController(there is only create method),Login,LoginResponse,UserRequest,UserResponse,UserService,UserServiceImpl(there is only loadUserByUsername(); method),UserMap,JwtTokenFilter,JwtTokenUtil,SecurityConfig,UserRepository

--- src/main/java/kg/peaksoft/peaksoftlmsb6/entity/User.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/entity/User.java
@@ -1,18 +1,24 @@
 package kg.peaksoft.peaksoftlmsb6.entity;
 
 import kg.peaksoft.peaksoftlmsb6.entity.enums.Role;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+@Builder
+@AllArgsConstructor
+public class User implements UserDetails, GrantedAuthority{
 
     @Id
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
@@ -28,4 +34,45 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> grantedAuthorities = new ArrayList<>();
+            grantedAuthorities.add(new SimpleGrantedAuthority(getRole().toString()));
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role.toString();
+    }
 }
