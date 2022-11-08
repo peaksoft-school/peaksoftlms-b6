f06f9169e34f13827908ec0b5ab2f4a6e2ca624f
Homework 9 <nkalilov@gmail.com>
2022-11-08 17:38:48 +0600

    Finished send email task

--- src/main/java/kg/peaksoft/peaksoftlmsb6/service/StudentService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/service/StudentService.java
@@ -13,9 +13,13 @@ import kg.peaksoft.peaksoftlmsb6.repository.GroupRepository;
 import kg.peaksoft.peaksoftlmsb6.repository.StudentRepository;
 import kg.peaksoft.peaksoftlmsb6.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,21 +33,37 @@ public class StudentService {
     private final PasswordEncoder passwordEncoder;
     private final GroupRepository groupRepository;
     private final UserRepository userRepository;
+    private final JavaMailSender javaMailSender;
 
-    public StudentResponse createStudent(StudentRequest studentRequest) {
-        studentRequest.setPassword(passwordEncoder.encode(studentRequest.getPassword()));
+    public StudentResponse createStudent(StudentRequest studentRequest) throws MessagingException {
         Group group = groupRepository.findById(studentRequest.getGroupId()).orElseThrow(
                 () -> new NotFoundException(String.format("Group not found with id=%s", studentRequest.getGroupId())));
         Student student = new Student(studentRequest);
         group.addStudents(student);
         student.setGroup(group);
+        sendEmail(student.getUser().getEmail());
+        student.getUser().setPassword(passwordEncoder.encode(studentRequest.getPassword()));
         studentRepository.save(student);
         return studentRepository.getStudent(student.getId());
     }
 
+    private SimpleResponse sendEmail(String email) throws MessagingException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException(String.format("Пользователь с email =%s не найден", email)));
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        messageHelper.setSubject("[peaksoftlms-b6] reset password link");
+        messageHelper.setFrom("peaksoftlms-b6@gmail.com");
+        messageHelper.setTo(email);
+        messageHelper.setText("Username: " + user.getUsername() + "\tPassword: " + user.getPassword(), true);
+        javaMailSender.send(mimeMessage);
+        return new SimpleResponse("Письмо успешно отправлено студенту");
+    }
+
+
     public StudentResponse update(Long id, StudentRequest studentRequest) {
         Student student = studentRepository.findById(id).orElseThrow(
-                () -> new NotFoundException(String.format("Student with id =%s not found", studentRequest.getId())));
+                () -> new NotFoundException(String.format("Student with id =%s not found", id)));
         Group group = groupRepository.findById(studentRequest.getGroupId()).orElseThrow(
                 () -> new NotFoundException(String.format("Group not found with id=%s", studentRequest.getGroupId())));
         student.setGroup(group);
